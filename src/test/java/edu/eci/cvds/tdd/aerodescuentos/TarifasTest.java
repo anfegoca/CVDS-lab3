4d783630a197f74029c13bf21a9208f3f27b73ae
unknown <andres.gonzalez-ca@labinfo.is.escuelaing.edu.co>
2020-02-05 11:51:29 -0500

    Falta implementacion

---
+++ src/test/java/edu/eci/cvds/tdd/aerodescuentos/TarifasTest.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.eci.cvds.tdd.aerodescuentos;
+
+
+/**
+ *
+ * @author andres.gonzalez-ca
+ */
+public class TarifasTest {
+    
+}
